2894583cd74f6d9ed322773f355d3d2e72006fc3
raresdolga <rares.dolga.16@ucl.ac.uk>
2017-11-02 17:20:53 +0000

    master

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates', 'static'],
+        'DIRS': ['logNote/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
